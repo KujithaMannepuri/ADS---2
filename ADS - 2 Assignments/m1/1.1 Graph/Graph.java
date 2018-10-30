221a8747660ac94b6ff4878fbf11a269e02f966f
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-30 18:06:29 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m1/1.1 Graph/Graph.java
+++ ADS - 2 Assignments/m1/1.1 Graph/Graph.java
@@ -5,14 +5,16 @@ interface Graph {
     public Iterable<Integer> arr(int v);
     public boolean hasEdge(int v, int w);
 }
+
 class GraphRep implements Graph {
-	int vertex;
+    int vertex;
     int edge;
     Bag<Integer>[] arr;
+
     GraphRep() {
 
     }
-    public GraphRep(int ver) {
+    GraphRep(int ver) {
         this.vertex = ver;
         this.edge = 0;
         arr = (Bag<Integer>[]) new Bag[ver];
@@ -20,73 +22,70 @@ class GraphRep implements Graph {
             arr[i] = new Bag<Integer>();
         }
     }
-	public int V() {
-		return vertex;
+    public int V() {
+        return vertex;
     }
-	public int E() {
-		return edge;
+    public int E() {
+        return edge;
     }
-
     public Iterable<Integer> arr(int v) {
         return arr[v];
     }
-
-	public void addEdge(int v, int w) {
-		if (v == w) {
-        	return;
+    public boolean hasEdge(int v, int w) {
+        for (int k : arr[v]) {
+            if (k == w) {
+                return true;
+            }
         }
-		if (!hasEdge(v,w)) {
-            edge++;
-
-            arr[v].add(w);
-            arr[w].add(v); 
-        }
+        return false;
     }
-	
-	public boolean hasEdge(int v, int w) {
-		for(int k :arr[v]) {
-				if (k==w) {
-					return true;
-				}
-		}
-		return false;
+    public void addEdge(int v, int w) {
+        if (v == w) {
+            return;
+        }
+        if (!hasEdge(v, w)) {
+            edge++;
+            arr[v].add(w);
+            arr[w].add(v);
+        }
     }
     public void matrixRep(int v, int e) throws Exception {
-    	if (e <= 1 && v <= 1) {
-    		System.out.println(V() + " vertices" + ", " + E() + " edges");
-    		throw new Exception("No edges");
-    	} else {
-    		System.out.println(V() + " vertices" + ", " + E() + " edges");
-    		int[][] mat = new int[v][v];
-    		for (int i = 0; i  < v; i++) {
-    			for (int j = 0; j < v; j++) {
-    				if (hasEdge(i, j)) {
-    					mat[i][j] = 1;
-		    		}
-    			}
-    		}
-    		for (int i = 0; i < v; i++) {
-    			for (int j = 0; j < v; j++) {
-    				System.out.print(mat[i][j] + " ");
-    			}
-    			System.out.println();
-    		}
-    	}
+        if (e <= 1 && v <= 1) {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            throw new Exception("No edges");
+        } else {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            int[][] mat = new int[v][v];
+            for (int i = 0; i  < v; i++) {
+                for (int j = 0; j < v; j++) {
+                    if (hasEdge(i, j)) {
+                        mat[i][j] = 1;
+                    }
+                }
+            }
+            for (int i = 0; i < v; i++) {
+                for (int j = 0; j < v; j++) {
+                    System.out.print(mat[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
     }
     public void listRep(int v, int e, String[] n1) throws Exception {
-    	if (e <= 1 && v <= 1) {
-    		System.out.println(V() + " vertices" + ", " + E() + " edges");
-    		throw new Exception("No edges");
-    	} else {
-    		System.out.println(V() + " vertices" + ", " + E() + " edges");
-    		for (int i = 0; i < n1.length; i++) {
-			String str = "";
-			str = n1[i] + ": ";
-			for (int k : arr(i)) {
-				str = str + n1[k] + " ";
-			}
-			System.out.println(str);
-			}
-    	}
+        if (e <= 1 && v <= 1) {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            throw new Exception("No edges");
+        } else {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            for (int i = 0; i < n1.length; i++) {
+            String str = "";
+            str = n1[i] + ": ";
+            for (int k : arr(i)) {
+                str = str + n1[k] + " ";
+            }
+            System.out.println(str);
+            }
+        }
     }
 }
+
--- ADS - 2 Assignments/m1/1.1 Graph/Solution.java
+++ ADS - 2 Assignments/m1/1.1 Graph/Solution.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-public class Solution {
+class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String token = sc.nextLine();
-		int v = Integer.parseInt(sc.nextLine());
-		int e = Integer.parseInt(sc.nextLine());
+		sc.nextLine();
+		int v = sc.nextInt();
+		int e = sc.nextInt();
 		String[] n1 = sc.nextLine().split(",");
 		GraphRep obj = new GraphRep(v);
-		while(sc.hasNext()) {
+		while (sc.hasNext()) {
 			String[] n2 = sc.nextLine().split(" ");
-			obj.addEdge(Integer.parseInt(n2[0]), Integer.parseInt(n2[1]));
+			obj.addEdge(Integer.parseInt(n2[0]),
+				Integer.parseInt(n2[1]));
 		}
 		switch (token) {
 			case "Matrix":
