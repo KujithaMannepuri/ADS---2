6d262dbbfe7efe593298a642adbec91059d8720d
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-30 18:24:31 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m1/1.1 Graph/Graph.java
+++ ADS - 2 Assignments/m1/1.1 Graph/Graph.java
@@ -1,3 +1,6 @@
+/**
+ * Interface for graph.
+ */
 interface Graph {
     public int V();
     public int E();
@@ -5,15 +8,33 @@ interface Graph {
     public Iterable<Integer> arr(int v);
     public boolean hasEdge(int v, int w);
 }
-
+/**
+ * Class for graph rep.
+ */
 class GraphRep implements Graph {
+    /**
+     * { var_description }.
+     */
     int vertex;
+    /**
+     * { var_description }.
+     */
     int edge;
+    /**
+     * { var_description }.
+     */
     Bag<Integer>[] arr;
-
+    /**
+     * Constructs the object.
+     */
     GraphRep() {
 
     }
+    /**
+     * Constructs the object.
+     *
+     * @param      ver   The version
+     */
     GraphRep(int ver) {
         this.vertex = ver;
         this.edge = 0;
@@ -22,15 +43,41 @@ class GraphRep implements Graph {
             arr[i] = new Bag<Integer>();
         }
     }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int V() {
         return vertex;
     }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int E() {
         return edge;
     }
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterable<Integer> arr(int v) {
         return arr[v];
     }
+    /**
+     * Determines if it has edge.
+     * time complexity O(E).
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
     public boolean hasEdge(int v, int w) {
         for (int k : arr[v]) {
             if (k == w) {
@@ -39,6 +86,13 @@ class GraphRep implements Graph {
         }
         return false;
     }
+    /**
+     * Adds an edge.
+     * Time complexity O(1).
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
     public void addEdge(int v, int w) {
         if (v == w) {
             return;
@@ -49,6 +103,16 @@ class GraphRep implements Graph {
             arr[w].add(v);
         }
     }
+
+    /**
+     * matrix representation.
+     * time complexity O(v).
+     *
+     * @param      v          { parameter_description }
+     * @param      e          { parameter_description }
+     *
+     * @throws     Exception  { exception_description }
+     */
     public void matrixRep(int v, int e) throws Exception {
         if (e <= 1 && v <= 1) {
             System.out.println(V() + " vertices" + ", " + E() + " edges");
@@ -71,6 +135,17 @@ class GraphRep implements Graph {
             }
         }
     }
+
+    /**
+     * list represenation.
+     * time complexity O(V+E).
+     *
+     * @param      v          { parameter_description }
+     * @param      e          { parameter_description }
+     * @param      n1         The n 1
+     *
+     * @throws     Exception  { exception_description }
+     */
     public void listRep(int v, int e, String[] n1) throws Exception {
         if (e <= 1 && v <= 1) {
             System.out.println(V() + " vertices" + ", " + E() + " edges");
