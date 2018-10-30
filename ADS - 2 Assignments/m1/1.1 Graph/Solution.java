d68a7ca6cc2239474cbc8f5f48eb8564c16717d6
KujithaMannepuri <kujitha120@msitprogram.net>
2018-10-30 18:08:06 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m1/1.1 Graph/Solution.java
+++ ADS - 2 Assignments/m1/1.1 Graph/Solution.java
@@ -1,36 +1,48 @@
 import java.util.Scanner;
 
-class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String token = sc.nextLine();
-		sc.nextLine();
-		int v = sc.nextInt();
-		int e = sc.nextInt();
-		String[] n1 = sc.nextLine().split(",");
-		GraphRep obj = new GraphRep(v);
-		while (sc.hasNext()) {
-			String[] n2 = sc.nextLine().split(" ");
-			obj.addEdge(Integer.parseInt(n2[0]),
-				Integer.parseInt(n2[1]));
-		}
-		switch (token) {
-			case "Matrix":
-			try {
-				obj.matrixRep(v, e);
-			} catch (Exception z) {
-				System.out.println(z.getMessage());
-			}
-			break;
-			case "List":
-			try {
-				obj.listRep(v, e, n1);
-			} catch (Exception z) {
-				System.out.println(z.getMessage());
-			}
-			break;
-			default :
-			break;
-		}
-	}
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constrcutor.
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String token = sc.nextLine();
+        int v = Integer.parseInt(sc.nextLine());
+        int e = Integer.parseInt(sc.nextLine());
+        String[] n1 = sc.nextLine().split(",");
+        GraphRep obj = new GraphRep(v);
+        while (sc.hasNext()) {
+            String[] n2 = sc.nextLine().split(" ");
+            obj.addEdge(Integer.parseInt(n2[0]),
+                Integer.parseInt(n2[1]));
+        }
+        switch (token) {
+            case "Matrix":
+            try {
+                obj.matrixRep(v, e);
+            } catch (Exception z) {
+                System.out.println(z.getMessage());
+            }
+            break;
+            case "List":
+            try {
+                obj.listRep(v, e, n1);
+            } catch (Exception z) {
+                System.out.println(z.getMessage());
+            }
+            break;
+            default :
+            break;
+        }
+    }
 }
+
+
