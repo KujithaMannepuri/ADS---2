cf5c9f3b4b6044a64bc9bde5b9792ff5a70229bb
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-01 00:27:25 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m2/2.1 CC_Percolation/Solution.java
+++ ADS - 2 Assignments/m2/2.1 CC_Percolation/Solution.java
@@ -1,18 +1,44 @@
 import java.util.Scanner;
 import java.util.Stack;
+/**
+ * Class for percolation.
+ */
 class Percolation {
 	boolean[][] grid;
+	/**
+	 * { var_description }.
+	 */
 	int first = 0;
+	/**
+	 * { var_description }.
+	 */
 	int last;
+	/**
+	 * { var_description }.
+	 */
 	int size;
+	/**
+	 * { var_description }.
+	 */
 	GraphRep gph;
-	Percolation(int n) {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     { parameter_description }
+	 */
+	Percolation(final int n) {
 		size = n;
         last = size * size + 1;
         gph = new GraphRep(size * size + 2);
         grid = new boolean[size][size];
 	}
-	public void open(int i,int j) {
+	/**
+	 * { function_description }.
+	 *
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 */
+	public void open(final int i, final int j) {
         grid[i-1][j-1] = true;
         if (i == 1) {
             gph.addEdge(component(i, j), first);
@@ -34,50 +60,115 @@ class Percolation {
             gph.addEdge(component(i, j), component(i + 1, j));
         }
     }
-	public boolean isOpen(int i, int j) {
+	/**
+	 * Determines if open.
+	 *
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 *
+	 * @return     True if open, False otherwise.
+	 */
+	public boolean isOpen(final int i, final int j) {
         return grid[i - 1][j - 1];
 	}
-	public boolean isFull(int i, int j) {
+	/**
+	 * Determines if full.
+	 *
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 *
+	 * @return     True if full, False otherwise.
+	 */
+	public boolean isFull(final int i, final int j) {
         if (0 < i && i <= size && 0 < j && j <= size) {
             return gph.hasEdge(first, component(i, j));
         } else {
             throw new IndexOutOfBoundsException();
         }
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public boolean percolates() {
         ConnectedComponent obj = new ConnectedComponent(gph,first);
         return obj.hasPathTo(last);
 	}
-	private int component(int i, int j) {
+	/**
+	 * { function_description }.
+	 *
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	private int component(final int i, final int j) {
         return size * (i - 1) + j;
     }
-
 }
+/**
+ * Class for connected component.
+ */
 class ConnectedComponent {
-	private boolean[] mark;
+	/**
+	 * { var_description }.
+	 */
     private int[] edge;
+    /**
+     * { var_description }.
+     */
     private final int sv;         // source vertex
-    public ConnectedComponent(GraphRep G, int s) {
+    /**
+     * { var_description }.
+     */
+    private boolean[] mark;
+    /**
+     * Constructs the object.
+     *
+     * @param      graph     { parameter_description }
+     * @param      s     { parameter_description }
+     */
+    public ConnectedComponent(final GraphRep graph, final int s) {
         this.sv = s;
-        edge = new int[G.V()];
-        mark = new boolean[G.V()];
+        edge = new int[graph.V()];
+        mark = new boolean[graph.V()];
         validateVertex(sv);
-        dfs(G, sv);
+        dfs(graph, sv);
     }
-
-    private void dfs(GraphRep G, int v) {
+    /**
+     * { function_description }.
+     *
+     * @param      graph     { parameter_description }
+     * @param      v     { parameter_description }
+     */
+    private void dfs(final GraphRep graph, final int v) {
         mark[v] = true;
-        for (int w : G.arr(v)) {
+        for (int w : graph.arr(v)) {
             if (!mark[w]) {
                 edge[w] = v;
-                dfs(G, w);
+                dfs(graph, w);
             }
         }
     }
-    public boolean hasPathTo(int v) {
+    /**
+     * Determines if it has path to.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     True if has path to, False otherwise.
+     */
+    public boolean hasPathTo(final int v) {
         validateVertex(v);
         return mark[v];
     }
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterable<Integer> pathTo(int v) {
         validateVertex(v);
         if (!hasPathTo(v)) return null;
@@ -87,14 +178,33 @@ class ConnectedComponent {
         path.push(sv);
         return path;
     }
-    private void validateVertex(int v) {
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     */
+    private void validateVertex(final int v) {
         int V = mark.length;
         if (v < 0 || v >= V)
             throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
     }
 }
-public class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		Percolation p = new Percolation(size);
