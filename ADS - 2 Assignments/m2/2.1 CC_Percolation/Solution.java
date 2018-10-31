e7f74880fcf6adbbd3688b1dcee945bab6645d91
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-01 00:39:13 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m2/2.1 CC_Percolation/Solution.java
+++ ADS - 2 Assignments/m2/2.1 CC_Percolation/Solution.java
@@ -4,44 +4,44 @@ import java.util.Stack;
  * Class for percolation.
  */
 class Percolation {
-	/**
-	 * { var_description }.
-	 */
-	boolean[][] grid;
-	/**
-	 * { var_description }.
-	 */
-	private int first = 0;
-	/**
-	 * { var_description }.
-	 */
-	private int last;
-	/**
-	 * { var_description }.
-	 */
-	private int size;
-	/**
-	 * { var_description }.
-	 */
-	private GraphRep gph;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      n     { parameter_description }
-	 */
-	Percolation(final int n) {
-		size = n;
+    /**
+     * { var_description }.
+     */
+    private boolean[][] grid;
+    /**
+     * { var_description }.
+     */
+    private int first = 0;
+    /**
+     * { var_description }.
+     */
+    private int last;
+    /**
+     * { var_description }.
+     */
+    private int size;
+    /**
+     * { var_description }.
+     */
+    private GraphRep gph;
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { parameter_description }
+     */
+    Percolation(final int n) {
+        size = n;
         last = size * size + 1;
         gph = new GraphRep(size * size + 2);
         grid = new boolean[size][size];
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      i     { parameter_description }
-	 * @param      j     { parameter_description }
-	 */
-	public void open(final int i, final int j) {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
+    public void open(final int i, final int j) {
         grid[i - 1][j - 1] = true;
         if (i == 1) {
             gph.addEdge(component(i, j), first);
@@ -63,50 +63,50 @@ class Percolation {
             gph.addEdge(component(i, j), component(i + 1, j));
         }
     }
-	/**
-	 * Determines if open.
-	 *
-	 * @param      i     { parameter_description }
-	 * @param      j     { parameter_description }
-	 *
-	 * @return     True if open, False otherwise.
-	 */
-	public boolean isOpen(final int i, final int j) {
+    /**
+     * Determines if open.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int i, final int j) {
         return grid[i - 1][j - 1];
-	}
-	/**
-	 * Determines if full.
-	 *
-	 * @param      i     { parameter_description }
-	 * @param      j     { parameter_description }
-	 *
-	 * @return     True if full, False otherwise.
-	 */
-	public boolean isFull(final int i, final int j) {
+    }
+    /**
+     * Determines if full.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     True if full, False otherwise.
+     */
+    public boolean isFull(final int i, final int j) {
         if (0 < i && i <= size && 0 < j && j <= size) {
             return gph.hasEdge(first, component(i, j));
         } else {
             throw new IndexOutOfBoundsException();
         }
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public boolean percolates() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean percolates() {
     ConnectedComponent obj = new ConnectedComponent(gph, first);
         return obj.hasPathTo(last);
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      i     { parameter_description }
-	 * @param      j     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	private int component(final int i, final int j) {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int component(final int i, final int j) {
         return size * (i - 1) + j;
     }
 }
@@ -114,9 +114,9 @@ class Percolation {
  * Class for connected component.
  */
 class ConnectedComponent {
-	/**
-	 * { var_description }.
-	 */
+    /**
+     * { var_description }.
+     */
     private int[] edge;
     /**
      * { var_description }.
@@ -175,7 +175,7 @@ class ConnectedComponent {
     public Iterable<Integer> pathTo(final int v) {
         validateVertex(v);
         if (!hasPathTo(v)) {
-        	return null;
+            return null;
         }
         Stack<Integer> path = new Stack<Integer>();
         for (int x = v; x != sv; x = edge[x]) {
@@ -193,7 +193,7 @@ class ConnectedComponent {
         int ver = mark.length;
         if (v < 0 || v >= ver) {
         throw new IllegalArgumentException("vertex "
-            	+ v + " is not between 0 and " + (ver - 1));
+                + v + " is not between 0 and " + (ver - 1));
     }
 }
 }
@@ -201,26 +201,26 @@ class ConnectedComponent {
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		Percolation p = new Percolation(size);
-		while (sc.hasNext()) {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        Percolation p = new Percolation(size);
+        while (sc.hasNext()) {
             int n1 = sc.nextInt();
             int n2 = sc.nextInt();
-			p.open(n1, n2);
-		}
-		System.out.println(p.percolates());
-	}
+            p.open(n1, n2);
+        }
+        System.out.println(p.percolates());
+    }
 }
