9720a7d37bb71965502fba058e0a18eedaccfa7b
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-04 23:39:53 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m3/3.2 Bipartite/Bag.java
+++ ADS - 2 Assignments/m3/3.2 Bipartite/Bag.java
@@ -8,82 +8,112 @@
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
+    /**
+     * variable num.
+     */
+    private int num;         // number of elements in bag
+    /**
+     * Node first.
+     */
     private Node first;    // beginning of bag
-
     // helper linked list class
+    /**
+     * Class for node.
+     */
     private class Node {
+        /**
+         * { var_description }.
+         */
         private Item item;
+        /**
+         * { var_description }.
+         */
         private Node next;
     }
-
    /**
      * Create an empty stack.
      */
     public Bag() {
         first = null;
-        N = 0;
+        num = 0;
     }
-
-   /**
-     * Is the BAG empty?
+    /**
+     * Is the BAG empty?.
+     *
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
-
-   /**
+    /**
      * Return the number of items in the bag.
+     *
+     * @return     { description_of_the_return_value }
      */
     public int size() {
-        return N;
+        return num;
     }
-
-   /**
-     * Add the item to the bag.
+    /**
+     * { function_description }.
+     *
+     * @param      item  The item
      */
-    public void add(Item item) {
+    public void add(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
-        N++;
+        num++;
     }
-
-
-   /**
+    /**
      * Return an iterator that iterates over the items in the bag.
+     *
+     * @return     { description_of_the_return_value }
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator();  
+        return new ListIterator();
     }
-
-    // an iterator, doesn't implement remove() since it's optional
+    // an iterator, doesn't implement remove() since it's optional.
+    /**
+     * Class for list iterator.
+     */
     private class ListIterator implements Iterator<Item> {
+        /**
+         * { var_description }.
+         */
         private Node current = first;
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * remove function.
+         */
+        public void remove() {
+        throw new UnsupportedOperationException();
+        }
+        /**
+         * { function_description }.
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
-
 }
