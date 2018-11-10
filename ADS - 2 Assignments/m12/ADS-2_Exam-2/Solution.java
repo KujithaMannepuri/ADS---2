ff984bf5f3e71692186fdbb67439f7bbd2eb6668
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-10 11:07:35 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS - 2 Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -27,6 +27,7 @@ public class Solution {
 			// First is the source and second is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
+			System.out.println("No Path Found. ");
 			break;
 
 		case "ViaPaths":
@@ -35,7 +36,7 @@ public class Solution {
 			// third is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
-			System.out.println("No Path Found");
+			System.out.println("No Path Found. ");
 			break;
 
 		default:
