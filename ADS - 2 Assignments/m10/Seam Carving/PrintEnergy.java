e63ac92886279e432e906c127093a5ef2877474d
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-11 23:40:19 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 2 Assignments/m10/Seam Carving/PrintEnergy.java
+++ ADS - 2 Assignments/m10/Seam Carving/PrintEnergy.java
@@ -1,17 +1,12 @@
-/******************************************************************************
- *  Compilation:  javac PrintEnergy.java
- *  Execution:    java PrintEnergy input.png
- *  Dependencies: SeamCarver.java
- *                
- *
- *  Read image from file specified as command line argument. Print energy
- *  of each pixel as calculated by SeamCarver object. 
- * 
- ******************************************************************************/
 /**
  * Class for print energy.
  */
 public class PrintEnergy {
+    /**
+     * Constructs the object.
+     */
+    private PrintEnergy() {
+    }
     /**
      * main function.
      *
@@ -19,7 +14,8 @@ public class PrintEnergy {
      */
     public static void main(final String[] args) {
         Picture picture = new Picture(args[0]);
-        StdOut.printf("image is %d pixels wide by %d pixels high.\n", picture.width(), picture.height());
+        StdOut.printf("image is %d pixels wide by %d pixels high.\n",
+            picture.width(), picture.height());
         SeamCarver sc = new SeamCarver(picture);
         StdOut.printf("Printing energy calculated for each pixel.\n");
         for (int row = 0; row < sc.height(); row++) {
