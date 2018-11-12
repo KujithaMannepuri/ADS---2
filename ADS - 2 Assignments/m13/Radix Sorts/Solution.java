78f5e7ffdb26ebe9b4232600e84da46bc505a811
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-12 15:04:27 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m13/Radix Sorts/Solution.java
+++ ADS - 2 Assignments/m13/Radix Sorts/Solution.java
@@ -4,7 +4,7 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		LSD lsd = new LSD();
-		int n = sc.nextInt();
+		int n = Integer.parseInt(sc.nextLine());
 		String[] arr = new String[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextLine();
