d7a4c4ca4bea3ae5eb892b5a7b621cfd7dc091ca
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-15 12:17:43 +0530

    "ads2_m16_a1_1: 2/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m16/boggle/BoggleSolver.java
+++ ADS - 2 Assignments/m16/boggle/BoggleSolver.java
@@ -13,6 +13,17 @@ public class BoggleSolver {
 	// Returns the score of the given word if it is in the dictionary, zero otherwise.
 	// (You can assume the word contains only the uppercase letters A through Z.)
 	public int scoreOf(String word) {
+		if (word.length() == 3 || word.length() == 4) {
+			return 1;
+		} else if (word.length() == 5) {
+			return 2;
+		} else if (word.length() == 6) {
+			return 3;
+		} else if (word.length() == 7) {
+			return 5;
+		} else if (word.length() >= 8) {
+			return 11;
+		}
 		return 0;
 	}
 }
