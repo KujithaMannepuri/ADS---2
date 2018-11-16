9b7a15ea41132bad67f78d30dd9c81178277a8c8
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-16 21:48:53 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 2 Assignments/m13/Radix Sorts/LSD.java
+++ ADS - 2 Assignments/m13/Radix Sorts/LSD.java
@@ -57,64 +57,64 @@ public class LSD {
      * time complexity - O(WN).
      * @param a the array to be sorted
      */
-    public static void sort(final int[] a) {
-        /**
-         * { var_description }.
-         */
-        final int bits = 32;                 // each int is 32 bits
-        /**
-         * { var_description }.
-         */
-        final int r1 = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
-        /**
-         * { var_description }.
-         */
-        final int mask = r1 - 1;              // 0xFF
-        /**
-         * { var_description }.
-         */
-        final int w = bits / BITS_PER_BYTE;  // each int is 4 bytes
-        /**
-         * { var_description }.
-         */
-        int n = a.length;
-        /**
-         * { var_description }.
-         */
-        int[] aux = new int[n];
-        for (int d = 0; d < w; d++) {
-            // compute frequency counts
-            int[] count = new int[r1 + 1];
-            for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
-                count[c + 1]++;
-            }
-            // compute cumulates
-            for (int r = 0; r < r1; r++) {
-                count[r + 1] += count[r];
-            }
-            // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
-            if (d == w - 1) {
-                int shift1 = count[r1] - count[r1 / 2];
-                int shift2 = count[r1 / 2];
-                for (int r = 0; r < r1 / 2; r++) {
-                    count[r] += shift1;
-                }
-                for (int r = r1 / 2; r < r1; r++) {
-                    count[r] -= shift2;
-                }
-            }
-            // move data
-            for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
-                aux[count[c]++] = a[i];
-            }
-            // copy back
-            for (int i = 0; i < n; i++) {
-                a[i] = aux[i];
-            }
-        }
-    }
+    // public static void sort(final int[] a) {
+    //     /**
+    //      * { var_description }.
+    //      */
+    //     final int bits = 32;                 // each int is 32 bits
+    //     /**
+    //      * { var_description }.
+    //      */
+    //     final int r1 = 1 << BITS_PER_BYTE;    // each bytes is between 0 and 255
+    //     /**
+    //      * { var_description }.
+    //      */
+    //     final int mask = r1 - 1;              // 0xFF
+    //     /**
+    //      * { var_description }.
+    //      */
+    //     final int w = bits / BITS_PER_BYTE;  // each int is 4 bytes
+    //     /**
+    //      * { var_description }.
+    //      */
+    //     int n = a.length;
+    //     /**
+    //      * { var_description }.
+    //      */
+    //     int[] aux = new int[n];
+    //     for (int d = 0; d < w; d++) {
+    //         // compute frequency counts
+    //         int[] count = new int[r1 + 1];
+    //         for (int i = 0; i < n; i++) {
+    //             int c = (a[i] >> BITS_PER_BYTE * d) & mask;
+    //             count[c + 1]++;
+    //         }
+    //         // compute cumulates
+    //         for (int r = 0; r < r1; r++) {
+    //             count[r + 1] += count[r];
+    //         }
+    //         // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
+    //         if (d == w - 1) {
+    //             int shift1 = count[r1] - count[r1 / 2];
+    //             int shift2 = count[r1 / 2];
+    //             for (int r = 0; r < r1 / 2; r++) {
+    //                 count[r] += shift1;
+    //             }
+    //             for (int r = r1 / 2; r < r1; r++) {
+    //                 count[r] -= shift2;
+    //             }
+    //         }
+    //         // move data
+    //         for (int i = 0; i < n; i++) {
+    //             int c = (a[i] >> BITS_PER_BYTE * d) & mask;
+    //             aux[count[c]++] = a[i];
+    //         }
+    //         // copy back
+    //         for (int i = 0; i < n; i++) {
+    //             a[i] = aux[i];
+    //         }
+    //     }
+    // }
 }
 
 
