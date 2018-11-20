6c0730f122ad0ac304ddfc58858b30fce05c81c7
KujithaMannepuri <kujitha120@msitprogram.net>
2018-11-20 17:48:33 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 2 Assignments/m3/3.1 Directed Cycle/Stack.java
+++ ADS - 2 Assignments/m3/3.1 Directed Cycle/Stack.java
@@ -29,7 +29,6 @@ public class Stack<Item> implements Iterable<Item> {
          */
         private Node next;
     }
-
    /**
      * Create an empty stack.
      */
@@ -37,32 +36,28 @@ public class Stack<Item> implements Iterable<Item> {
         first = null;
         N = 0;
     }
-
    /**
      * Is the stack empty?
      */
     public boolean isEmpty() {
         return first == null;
     }
-
    /**
      * Return the number of items in the stack.
      */
     public int size() {
         return N;
     }
-
    /**
      * Add the item to the stack.
      */
-    public void push(Item item) {
+    public void push(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
         N++;
     }
-
    /**
      * Delete and return the item most recently added to the stack.
      * Throw an exception if no such item exists because the stack is empty.
@@ -74,8 +69,6 @@ public class Stack<Item> implements Iterable<Item> {
         N--;
         return item;                   // return the saved item
     }
-
-
    /**
      * Return the item most recently added to the stack.
      * Throw an exception if no such item exists because the stack is empty.
@@ -84,7 +77,6 @@ public class Stack<Item> implements Iterable<Item> {
         if (isEmpty()) throw new RuntimeException("Stack underflow");
         return first.item;
     }
-
    /**
      * Return string representation.
      */
@@ -94,23 +86,47 @@ public class Stack<Item> implements Iterable<Item> {
             s.append(item + " ");
         return s.toString();
     }
-       
-
    /**
-     * Return an iterator to the stack that iterates through the items in LIFO order.
+     * Return an iterator to the stack that iterates through the
+     * items in LIFO order.
      */
-    public Iterator<Item> iterator()  { return new ListIterator();  }
-
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+    /**
+     * Class for list iterator.
+     */
     // an iterator, doesn't implement remove() since it's optional
     private class ListIterator implements Iterator<Item> {
+        /**
+         * { var_description }.
+         */
         private Node current = first;
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * { function_description }.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * { function_description }.
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
